a7c2d0197b8bd19b9e77bddd7f2bfb01611c5aad
vidhi <vanshpratik0165@gmail.com>
2022-08-26 11:30:38 +0530

    add more banner

--- components/Banner.js
+++ components/Banner.js
@@ -1,33 +1,34 @@
 import React from 'react'
-import {Carousel} from 'react-responsive-carousel'
+import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles//carousel.min.css'
 const bannerUrl = [
-    'http://surl.li/cjqce',
-    'http://surl.li/cjqbi',
-    'http://surl.li/cjqbs',
-    'https://m.media-amazon.com/images/I/71YfmYhUXzL._SX3000_.jpg',
-    'https://m.media-amazon.com/images/I/61gFPL8x3+L._SX3000_.jpg',
-    'https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/SingleTitle/SamratPrithviraj-PD/3000x1200_Hero-Tall_NP._CB632351883_.jpg'
-  ]
+  'https://m.media-amazon.com/images/I/61aURrton0L._SX3000_.jpg',
+  'https://m.media-amazon.com/images/I/61HMg7wnMZL._SX3000_.jpg',
+  'https://m.media-amazon.com/images/I/71eQVahI6GL._SX3000_.jpg',
+  'https://m.media-amazon.com/images/I/61BsKK5N4zL._SX3000_.jpg',
+  'https://m.media-amazon.com/images/I/71YfmYhUXzL._SX3000_.jpg',
+  'https://m.media-amazon.com/images/I/61gFPL8x3+L._SX3000_.jpg',
+  'https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/SingleTitle/SamratPrithviraj-PD/3000x1200_Hero-Tall_NP._CB632351883_.jpg'
+]
 const Banner = () => {
   return (
     <div className='mx-auto'>
-        <Carousel
-            autoPlay
-            infiniteLoop
-            showStatus={false}
-            showIndicators={false}
-            showThumbs={false}
-            interval={5000}
-        >
-                {
-                    bannerUrl.map(url=>(
-                        <div key={url}>
-                            <img src={url} loading='lazy'/>
-                        </div>
-                    ))
-                }
-        </Carousel>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        {
+          bannerUrl.map(url => (
+            <div key={url}>
+              <img src={url} loading='lazy' />
+            </div>
+          ))
+        }
+      </Carousel>
     </div>
   )
 }
