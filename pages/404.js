50785b6e66b059874ff26f1e175316a2ea2c4d95
vidhi <vanshpratik0165@gmail.com>
2022-09-09 11:52:25 +0530

    404

--- pages/404.js
+++ pages/404.js
@@ -2,6 +2,7 @@
 const ErrorPage = () => {
   return (
     <div className="flex h-screen">
+      <p>404</p>
       <h1 className="flex items-center justify-center mx-auto">this is coustom error page</h1>
     </div>
   )
