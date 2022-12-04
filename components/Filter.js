3b597e62ed80086828179f98ed3781f9d2ffed19
pbvansh <vanshpratik0165@gmail.com>
2022-12-04 16:54:24 +0530

    add product not found img

--- components/Filter.js
+++ components/Filter.js
@@ -3,10 +3,10 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 
 const Filter = ({setURL}) => {
-    const [catName,setCatName] = useState(null)
-    const [Min,setMin] = useState(null)
-    const [Max,setMax] = useState(null)
-    const [Product,setProduct] = useState(null)
+    const [category,setCatName] = useState(null)
+    const [Min,setMin] = useState(0)
+    const [Max,setMax] = useState(5000)
+    const [name,setProduct] = useState(null)
     const [cat, setCat] = useState([])
     useEffect(() => {
         axios.get('https://aaumartbackend.pratikvansh.repl.co/api/category')
@@ -27,8 +27,8 @@ const Filter = ({setURL}) => {
     const ApplyFilter =(e)=>{
         e.preventDefault();
         let options = '';
-        if(catName){
-             options += `cat=${catName}&`;
+        if(category){
+             options += `category=${category}&`;
         }
         if(Min){
              options += `Min=${Min}&`;
@@ -36,8 +36,8 @@ const Filter = ({setURL}) => {
         if(Max){
              options += `Max=${Max}&`;
         }
-        if(Product){
-             options += `Product=${Product}&`;
+        if(name){
+             options += `name=${name}&`;
         }
         console.log(options);
         setURL(`https://aaumartbackend.pratikvansh.repl.co/api/product?${options}`)
@@ -49,7 +49,7 @@ const Filter = ({setURL}) => {
             </div>
             <div className="p-5 space-y-3">
                 <p className="font-bold text-sm">CATEGORIES</p>
-                <select value={catName} onChange={(e)=>setCatName(e.target.value)} className="focus:outline-none border p-[9px] text-sm w-full rounded-sm" name="state">
+                <select value={category} onChange={(e)=>setCatName(e.target.value)} className="focus:outline-none border p-[9px] text-sm w-full rounded-sm" name="state">
                     <option value="All">--All--</option>
                     {
                         cat.map(({ name }) => <option className="" value={name}>{name}</option>)
@@ -72,7 +72,7 @@ const Filter = ({setURL}) => {
                         <option value={500}>₹500</option>
                         <option value={1000}>₹1000</option>
                         <option value={2000}>₹2000</option>
-                        <option selected value={5000}>₹5000</option>
+                        <option value={5000}>₹5000</option>
                     </select>
                 </div>
             </div>
@@ -80,7 +80,7 @@ const Filter = ({setURL}) => {
                 <p className="font-bold text-sm">Product</p>
                 <div className="flex items-center">
                     <SearchIcon className="h-4 text-gray-400" />
-                    <input value={Product} onChange={(e)=>setProduct(e.target.value)} type={'text'} placeholder='Search Product' className="outline-none border-b-2 focus:border-b-blue-400 duration-500 ease-in-out text-sm p-1" />
+                    <input value={name} onChange={(e)=>setProduct(e.target.value)} type={'text'} placeholder='Search Product' className="outline-none border-b-2 focus:border-b-blue-400 duration-500 ease-in-out text-sm p-1" />
                 </div>
             </div>
             <div className="flex justify-end p-5">
--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -6,7 +6,7 @@ import Product from './Product';
 const ProductFeed = () => {
 
   const [products, setProducts] = useState([])
-  const [URL,setURL] = useState('https://aaumartbackend.pratikvansh.repl.co/api/product');
+  const [URL, setURL] = useState('https://aaumartbackend.pratikvansh.repl.co/api/product');
 
   useEffect(() => {
     axios.get(URL).then((prod) => {
@@ -17,24 +17,31 @@ const ProductFeed = () => {
   return (
     <div className='mx-auto'>
       <div className='flex'>
-      <Filter setURL={setURL}/>
-        <div className='grid grid-flow-row-dense sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-          {
-            products && (
-              products.map((prod) => (
-                <Product
-                  key={prod._id}
-                  id={prod._id}
-                  name={prod.name}
-                  desc={prod.desc}
-                  catname={prod.category}
-                  price={prod.price}
-                  url={prod.img_url}
-                />
-              ))
-            )
-          }
-        </div>
+        <Filter setURL={setURL} />
+        {products.length > 0 ?
+          <div className='grid grid-flow-row-dense sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {
+              products && (
+                products.map((prod) => (
+                  <Product
+                    key={prod._id}
+                    id={prod._id}
+                    name={prod.name}
+                    desc={prod.desc}
+                    catname={prod.category}
+                    price={prod.price}
+                    url={prod.img_url}
+                  />
+                ))
+              )
+            }
+          </div>
+          :
+          <div className='flex items-center justify-center '>
+            <img src='item-not-found.png'/>
+          </div>
+        }
+
       </div>
     </div>
   )
